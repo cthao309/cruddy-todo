3540f5fccfbfc1b3c057a2d56af01e7c6e3a8359
Charlie Thao <cthao309@gmail.com>
2020-04-13 12:54:57 -0700

    refactored delete method

--- datastore/index.js
+++ datastore/index.js
@@ -91,14 +91,13 @@ exports.update = (id, text, callback) => {
 };
 
 exports.delete = (id, callback) => {
-  var item = items[id];
-  delete items[id];
-  if (!item) {
-    // report an error if item not found
-    callback(new Error(`No item with id: ${id}`));
-  } else {
-    callback();
-  }
+  // grab the file path by the id
+  let filePath = path.join(exports.dataDir, `${id}.txt`);
+
+  // remove the file using fs.unlink
+  fs.unlink(filePath, (error) => {
+    callback(error)
+  });
 };
 
 // Config+Initialization code -- DO NOT MODIFY /////////////////////////////////
