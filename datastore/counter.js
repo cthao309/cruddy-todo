93de74047c656ca7f6fad63b9ac2352e6f14d786
Charlie Thao <cthao309@gmail.com>
2020-04-13 12:37:53 -0700

    finished refactored counter and create

--- datastore/counter.js
+++ datastore/counter.js
@@ -38,9 +38,12 @@ const writeCounter = (count, callback) => {
 
 // Public API - Fix this function //////////////////////////////////////////////
 
-exports.getNextUniqueId = () => {
-  counter = counter + 1;
-  return zeroPaddedNumber(counter);
+exports.getNextUniqueId = (callback) => {
+  readCounter((error, count) => {
+    writeCounter(count + 1, (error, id) => {
+      callback(error, id);
+    });
+  });
 };
 
 
--- datastore/index.js
+++ datastore/index.js
@@ -3,30 +3,77 @@ const path = require('path');
 const _ = require('underscore');
 const counter = require('./counter');
 
-var items = {};
+// load bluebird module for asynchronous task
+const Promise = require('bluebird');
+const readFilePromise = Promise.promisify(fs.readFile);
 
 // Public API - Fix these CRUD functions ///////////////////////////////////////
 
 exports.create = (text, callback) => {
-  var id = counter.getNextUniqueId();
-  items[id] = text;
-  callback(null, { id, text });
+  counter.getNextUniqueId((err, id) => {
+    // specify the path with the unique id as it name
+    let filePath = path.join(exports.dataDir, `${id}.txt`);
+
+    console.log('file path => ', filePath)
+
+    // write to the file with only the text
+    fs.writeFile(filePath, text, (error) => {
+      if(error) {
+        callback(error);
+      } else {
+        callback(null, {id, text });
+      }
+    });
+  });
 };
 
 exports.readAll = (callback) => {
-  var data = _.map(items, (text, id) => {
-    return { id, text };
+  // look through the directory call dataDir
+  fs.readdir(exports.dataDir, (error, files) => {
+    if(error) {
+      throw ('No such data file');
+    }
+
+    // map through the files
+    let data = _.map(files, (file) => {
+
+      // find the file name that end with .txt
+      console.log('file => ', file)
+
+      let id = path.basename(file, '.txt');
+
+      // get the file path
+      let filePath = path.join(exports.dataDir, file);
+
+      console.log('file path in readall => ', filePath)
+
+      // return all file with its id and text
+      return readFilePromise(filePath).then(fileData => {
+        return {
+          id: id,
+          text: fileData.toString()
+        };
+      });
+    });
+
+    // apply all the callback to all the data file
+    Promise.all(data)
+      .then(items => callback(null, items), error => callback(error));
   });
-  callback(null, data);
 };
 
 exports.readOne = (id, callback) => {
-  var text = items[id];
-  if (!text) {
-    callback(new Error(`No item with id: ${id}`));
-  } else {
-    callback(null, { id, text });
-  }
+  // grab the file path based off of id
+  let filePath = path.join(exports.dataDir, `${id}.txt`)
+
+  // read the file
+  fs.readFile(filePath, (error, fileData) => {
+    if(error) {
+      callback(error);
+    } else {
+      callback(null, { id, text: fileData.toString() });
+    }
+  })
 };
 
 exports.update = (id, text, callback) => {
